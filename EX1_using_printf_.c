07395d5b2b4cf77819e79a875bd3346362d22ad5
eslam mostafa <92211313+XDanger65@users.noreply.github.com>
2023-07-20 20:45:41 +0300

    Add files via upload

---
+++ EX1_using_printf_.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : EX1_using_printf_.c
+ Author      : ESLAM MOSTAFA MOHAMED
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("C Programming \n");
+	return EXIT_SUCCESS;
+}
